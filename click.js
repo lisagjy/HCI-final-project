7fddaf5e59965bc5ce26727c8d61ecb09f698dfc
Jiayi Ge <63018086+lisagjy@users.noreply.github.com>
2023-11-26 22:27:40 -0500

    Click Test Finished minus proper instruction
    1)Added instruction+start button section for click test
    2)The downloaded file is now completionTime + distance to the dot + accuracyScore
    3) Accuracy implemented in click test

--- click.js
+++ click.js
@@ -28,56 +28,68 @@ function renderDot() {
 function deleteDot() {
   const dot = document.getElementById('dot');
   dot.style.display = 'none';
-  //dot.parentNode.removeChild(dot);
 }
 
 
-function handleClick() {
+function handleClick(event) {
   endTime = new Date();
   const backButton = document.getElementById('backButton');
   const timeDiff = endTime - startTime; // in milliseconds
+  const dot = document.getElementById('dot');
+  const dot_rect = dot.getBoundingClientRect();
+  const clicked_x = event.clientX;
+  const clicked_y = event.clientY;
+  const dot_x = dot_rect.left + dot_rect.width / 2;
+  const dot_y = dot_rect.top + dot_rect.height / 2;
+  const tolerance = 10;
 
   // console.log(`Click ${clickCount + 1}: ${timeDiff}ms (${accuracy})`);
 
+  var distance = getDistance(dot_x,dot_y,clicked_x,clicked_y);
+  if (distance > dot_rect.width/2+tolerance){
+    return;
+  }
   clickCount++;
-
   if (clickCount < 2) {
     deleteDot();
     renderDot();
-    // trial = timeDiff + "\n";
-    // data += trial;
-    trial.push(timeDiff);
+    const accuracy = Math.max(0,1-distance/(dot_rect.width/2));
+    trial.push(timeDiff + "," +distance+","+accuracy);
   } else {
     deleteDot();
-    // trial = timeDiff + "\n";
-    // data += trial;
-    trial.push(timeDiff);
+    dot.parentNode.removeChild(dot);
+    const accuracy = Math.max(0,1-distance/(dot_rect.width/2));
+    trial.push(timeDiff + "," +distance+","+accuracy);
     data = trial.join("\n");
     console.log('Test completed!');
     console.log(data);
     backButton.style.display = 'inline-flex';
     clickCount = 0;
     saveAndDownload();
+    document.removeEventListener('click', handleClick);
   }
 }
+
+function getDistance(x1,y1,x2,y2){
+  return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+}
+
 function saveAndDownload() {
   const name = localStorage.getItem('userName');
   var blob = new Blob([data], { type: "text/plain" });
   var link = document.createElement("a");
   link.download = name + "_"+ "click.txt";
   link.href = URL.createObjectURL(blob);
-
-  // Append the link to the body
   document.body.appendChild(link);
-
-  // Trigger a click on the link to start the download
   link.click();
-
-  // Remove the link from the DOM
   document.body.removeChild(link);
 }
 
-document.getElementById('dot').addEventListener('click', handleClick);
+function startClickTest(){
+  document.body.removeChild(document.getElementById("click-instruction"));
+  renderDot();
+  document.addEventListener('click', handleClick)
+}
 
-// Start the test
-renderDot();
+// document.getElementById('dot').addEventListener('click', handleClick);
+deleteDot();
